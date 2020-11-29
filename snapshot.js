6d70fe878b4980156cef051c6463e6befa777102
eggplantiny <eggplantiny@gmail.com>
2020-11-29 23:15:48 +0900

    wasm ffmpeg 적용

--- snapshot.js
+++ snapshot.js
@@ -1,7 +1,8 @@
 
-const ffmpeg = require('fluent-ffmpeg')
+const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg')
 const path = require('path')
 const fs = require('fs')
+const ffmpeg = createFFmpeg({ log: true })
 
 const folder = 'D:\\download\\torrent'
 const fileName = 'FC2-PPV-802311'
@@ -14,34 +15,11 @@ function makeSnapshotFolder (fileName) {
     return targetPath
 }
 
-function takeSnapShot (folder, fileName, snapshotFolder) {
-    return new Promise((resolve, reject) => {
-        ffmpeg(path.resolve(`${folder}\\${fileName}.mp4`))
-            .inputOption('-vframes 1')
-            .on('filenames', function (filenames) {
-                console.log('screenshots are ' + filenames.join(', '))
-            })
-            .on('end', function() {
-                console.log('screenshots were saved')
-                return resolve()
-            })
-            .on('error', function(err) {
-                return reject(err)
-            })
-            .screenshots({
-                count: 100,
-                size: '1080x?',
-                folder: snapshotFolder,
-                finename: `${fileName}_%s.png`
-            })
-    })
-}
-
 async function run () {
     const snapshotFolder = makeSnapshotFolder(fileName)
     try {
         console.time('snapshot')
-        await takeSnapShot(folder, fileName, snapshotFolder)
+        // await ffmpeg.FS('writeFile', fileName, awa)
         console.timeEnd('snapshot')
     } catch (e) {
         console.log('an error happened: ' + e.message)
