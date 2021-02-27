0a668a87f48f31b1908616e5b3c6272c77b32138
eggplantiny <eggplantiny@gmail.com>
2021-02-27 21:02:12 +0900

    list 생성 및 pagination설정

--- file-manager/src/frontend/pages/index.vue
+++ file-manager/src/frontend/pages/index.vue
@@ -1,12 +1,27 @@
 <template>
   <v-row>
     <v-col cols="12">
-      Hello World
+        <v-list
+        >
+          <template
+            v-for="(item, index) in targetItemList"
+          >
+          <v-list-item
+            :key="index"
+            @click="onClickItem(item)"
+          >
+            <v-list-item-title>
+              {{ item }}
+            </v-list-item-title>
+          </v-list-item>
+          </template>
+        </v-list>
     </v-col>
     <v-col cols="12">
-      <v-btn @click="readFileList">
-        Load File List
-      </v-btn>
+      <v-pagination
+        v-model="page"
+        :length="pageLength"
+      />
     </v-col>
   </v-row>
 </template>
@@ -14,13 +29,34 @@
 <script>
 import { ipcRenderer } from 'electron'
 
+const ITEM_PER_PAGE = 30
+
 export default {
   name: 'Home',
-  methods: {
-    async readFileList () {
-      const asd = await ipcRenderer.invoke('read-file-list')
-      console.log(asd)
+  data () {
+    return {
+      itemList: [],
+      page: 0
     }
+  },
+  computed: {
+    targetItemList () {
+      const { itemList, page } = this
+      return itemList.slice(page * ITEM_PER_PAGE, page * ITEM_PER_PAGE + ITEM_PER_PAGE)
+    },
+    pageLength () {
+      const { itemList } = this
+      return Math.floor(itemList.length / ITEM_PER_PAGE)
+    }
+  },
+  methods: {
+    onClickItem (item) {
+      console.log(item)
+    }
+  },
+  async mounted () {
+    const fileList = await ipcRenderer.invoke('read-file-list')
+    this.itemList.push(...fileList)
   }
 }
 </script>
